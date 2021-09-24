78346b1eca17df7d3ea6a1967eed240c47e25158
ukaran11 <ukaran11@gmail.com>
2021-09-25 02:58:56 +0530

    Utsav | Added basic layout for search using axios

---
+++ src/useBookSearch.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+export default function useBookSearch(query, pageNumber) {
+
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'http://openlibrary.org/search.json',
+            params: {q: query, page: pageNumber}
+        }).then(res => {
+            console.log(res.data)
+        })
+    }, [query, pageNumber])
+    return null;
+}
